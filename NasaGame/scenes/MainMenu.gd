95349c27653d0bb118d05402474ea2915c5cdf9f
大卫 <69019450+dlimthe25@users.noreply.github.com>
2024-10-06 11:18:33 +0800

    Main menu

--- NasaGame/scenes/MainMenu.gd
+++ NasaGame/scenes/MainMenu.gd
@@ -1,16 +1,26 @@
 extends Control
 
 func _on_start_game_pressed():
+	# Play the click sound
+	$ClickSoundPlayer.play()
+	
 	# Load the game scene as a PackedScene and change to it
 	var game_scene = load("res://scenes/game.tscn") as PackedScene
 	if game_scene != null:
 		get_tree().change_scene_to_packed(game_scene)
 
 func _on_quit_pressed():
-	# Quit the game
+	# Play the click sound
+	$ClickSoundPlayer.play()
+
+	# Quit the game after a slight delay to allow the click sound to play
+	await get_tree().create_timer(0.2).timeout
 	get_tree().quit()
 
-# Connect buttons in the _ready() function
 func _ready():
 	$VBoxContainer/Start.connect("pressed", Callable(self, "_on_start_game_pressed"))
 	$VBoxContainer/Quit.connect("pressed", Callable(self, "_on_quit_pressed"))
+	
+	# Start the background music
+	$"Background music".stream.loop = true
+	$"Background music".play()
